f7b60b3e0fc7ef369b4b7ac9fc4539e879953fa9
wherewhere <qq2518766683@outlook.com>
2022-10-31 18:54:54 +0800

    Create Project

---
+++ Loopback/MainWindow.xaml.cs
@@ -0,0 +1,130 @@
+﻿using ModernWpf.Controls;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Input;
+using MessageBox = ModernWpf.Controls.MessageBox;
+
+namespace Loopback
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private LoopUtil _loop;
+        private List<LoopUtil.AppContainer> appFiltered = new List<LoopUtil.AppContainer>();
+        private bool isDirty = false;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            _loop = new LoopUtil();
+            dgLoopback.ItemsSource = appFiltered;
+            Filter(string.Empty, false, null);
+            ICollectionView cvApps = CollectionViewSource.GetDefaultView(dgLoopback.ItemsSource);
+        }
+
+        private void btnRefresh_Click(object sender, RoutedEventArgs e)
+        {
+            _loop.LoadApps();
+            Filter(string.Empty, false, null);
+            txtFilter.Text = "";
+            Loopback_Enabled.IsChecked = false;
+            Loopback_Disabled.IsChecked = false;
+            isDirty = false;
+            Log("refreshed");
+        }
+
+        private void btnSave_Click(object sender, RoutedEventArgs e)
+        {
+            if (!isDirty)
+            {
+                Log("nothing to save");
+                return;
+            }
+
+            isDirty = false;
+            if (_loop.SaveLoopbackState())
+            {
+                Log(" saved loopback excemptions");
+            }
+            else
+            {
+                Log(" ERROR SAVING");
+            }
+        }
+
+        private void dgcbLoop_Click(object sender, RoutedEventArgs e)
+        {
+            isDirty = true;
+        }
+
+        private void Filter(string filter, bool Ischecked, bool? IsEnabled)
+        {
+            string appsInFilter = filter.ToUpper();
+            appFiltered.Clear();
+
+            foreach (LoopUtil.AppContainer app in _loop.Apps)
+            {
+                string appName = app.DisplayName.ToUpper();
+
+                if (string.IsNullOrEmpty(filter) || appName.Contains(appsInFilter))
+                {
+                    if (Ischecked == false || app.LoopUtil == IsEnabled)
+                    {
+                        appFiltered.Add(app);
+                    }
+                }
+            }
+            dgLoopback.Items.Refresh();
+        }
+
+        private void Log(string logtxt)
+        {
+            txtStatus.Text = DateTime.Now.ToString("hh:mm:ss.fff ") + logtxt;
+        }
+
+        private void Loopback_Click_Disabled(object sender, RoutedEventArgs e)
+        {
+            Filter(txtFilter.Text, (bool)Loopback_Disabled.IsChecked, false);
+            Loopback_Enabled.IsChecked = false;
+        }
+
+        private void Loopback_Click_Enabled(object sender, RoutedEventArgs e)
+        {
+            Filter(txtFilter.Text, (bool)Loopback_Enabled.IsChecked, true);
+            Loopback_Disabled.IsChecked = false;
+        }
+
+        private void txtFilter_KeyUp(object sender, KeyEventArgs e)
+        {
+            bool isEnabledChecked = (bool)Loopback_Enabled.IsChecked;
+            if (isEnabledChecked)
+            {
+                Filter(txtFilter.Text, (bool)Loopback_Enabled.IsChecked, isEnabledChecked);
+            }
+            else
+            {
+                Filter(txtFilter.Text, (bool)Loopback_Disabled.IsChecked, isEnabledChecked);
+            }
+        }
+
+        private void Window_Closing(object sender, CancelEventArgs e)
+        {
+            if (isDirty)
+            {
+                MessageBoxResult resp = MessageBox.Show("You have not saved your changes. Are you sure you want to exit ?", "Loopback Manager", MessageBoxButton.YesNo);
+                if (resp == MessageBoxResult.No)
+                {
+                    e.Cancel = true;
+                    return;
+                }
+            }
+            //To Do
+            _loop.FreeResources();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,26 @@
+# Project Description
+
+This project is enhancement of [Windows Loopback Exemption Manager](https://github.com/tiagonmas/Windows-Loopback-Exemption-Manager).
+
+GUI to enable Loopback Exemptions for Universal Windows Apps and Windows 8, 8.1, 10 Modern UI Apps.
+
+By default, Windows Modern UI and Universal Apps are forbidden to send network traffic to the local Computer. In order to debug Apps with a tool, we need to enable Loopback capabilities for those Apps.
+This tool enables the management of the Apps that can connect to the local Computer.
+
+# More information about this topic
+- [Revisiting Fiddler and Win8+ Immersive applications](https://docs.microsoft.com/en-us/archive/blogs/fiddler/revisiting-fiddler-and-win8-immersive-applications])
+- [Fiddler and Windows 8 Metro-style applications](https://docs.microsoft.com/en-us/archive/blogs/fiddler/fiddler-and-windows-8-metro-style-applications)
+- [How to enable loopback and troubleshoot network isolation (Windows Store apps)](https://docs.microsoft.com/en-us/previous-versions/windows/apps/hh780593(v=win.10))
+
+# Network Isolation APIs
+Check the source code of this project on a sample usage of the Network Isolation APIs : 
+NetworkIsolationEnumAppContainers 
+NetworkIsolationFreeAppContainers 
+NetworkIsolationGetAppContainerConfig 
+NetworkIsolationSetAppContainerConfig
+
+![Loopback Exemption Manager](LoopbackSampleExecution.png)
+
+This tool does basically the same thing as Fiddler EnableLoopback Utility.
+
+Thanks to [Eric Lawrence](http://stackoverflow.com/users/126229/ericlaw) for helping with the PInvokes.
